b69b8d9d1c56fac34a38ba4e56d0d6a5258806cb
FreeMasen <RFM@Herman.local>
2017-01-07 21:33:14 -0600

    git ignore magic

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+
+const app = express()
+
+var posts = generatePosts()
+
+function generatePosts() {
+    var ret = []
+    var lorem = `On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.`
+    
+    var dateCounter = 0
+    for (i=0;i<10;i++) {
+      dateCounter += 1
+      ret.push({
+        _id: i,
+        title: `${i}: recipe`,
+        type: 'recipe',
+        postDate: new Date(2017,01,dateCounter),
+        contents: lorem,
+        images: ['assets/images/test.jpg']
+      })
+      dateCounter += 1
+      ret.push({
+        _id: i+10,
+        title: `${i} review`,
+        type: 'review',
+        postDate: new Date(2017, 01, dateCounter),
+        contents: lorem,
+        images: ['assets/images/test.jpg']
+      })
+    }
+    return ret
+}
+
+app.use(express.static(`${__dirname}`))
+
+app.get('/posts/:type', (req, res) => {
+  var sorted = 
+  res.send(JSON.stringify(posts.sort((a, b) => {
+    return a.postDate / 1000 - b.postDate / 1000
+  })))
+})
+
+
+
+app.get('*', (req, res) => {
+  res.redirect('/')
+})
+
+app.listen(1211, err => {
+  if (err) {
+    console.log(err.message)
+    process.exit()
+  }
+  console.log('listening on 1211')
+})

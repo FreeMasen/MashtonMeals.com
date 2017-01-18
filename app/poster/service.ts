aab998ad027cf7403da241d4c063965553d66679
FreeMasen <RFM@Herman.local>
2017-01-17 20:04:30 -0600

    Add upload image function

--- app/poster/service.ts
+++ app/poster/service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { Http, Response } from '@angular/http'
+import { Http, Request, Response, RequestOptions, Headers } from '@angular/http'
 import { Observable } from 'rxjs'
 import 'rxjs/add/operator/toPromise'
 
@@ -7,6 +7,13 @@ import { Post } from '../models/post'
 
 @Injectable()
 export class Poster {
+
+    private reader: FileReader = new FileReader()
+    private queue: RequestOptions[] = []
+    private imageHeaders: Headers = new Headers({
+        'Content-Type': 'application/octet-stream'
+    })
+    private clearingQueue: boolean = false
     constructor(private http: Http) {}
 
     get(type: string = undefined): Promise<Post[]> {
@@ -28,6 +35,51 @@ export class Poster {
                                     , p.contents
                                     , p.images)
                 })
+            })
+    }
+
+    uploadImages(images: File[]) {
+        console.log(images);
+        for (var i = 0;i<images.length;i++) {
+            this.queue.push(new RequestOptions({
+                method: "POST",
+                url: `/new/image/${images[i].name}`,
+                headers: this.imageHeaders,
+                body: images[i]
+            }))
+        }
+        if (!this.clearingQueue) {
+            this.clearQueue()
+        }
+    }
+    
+    clearQueue() {
+        var self = this
+        if (self.queue && self.queue.length > 0) {
+            self.clearingQueue = true
+            var req = new Request(self.queue.slice(0,1)[0])
+            self.http.request(req)
+                .toPromise()
+                .then(response => {
+                    if (self.queue.length > 0) {
+                        setTimeout(self.clearQueue, 1)
+                    } else {
+                        self.clearingQueue = false
+                    }
+                })
+                .catch(error => {
+                    self.clearingQueue = false
+                    return error
+                })
+        }
+    }
+
+    post(post: Post) {
+        return this.http.post(`new/post`,
+            post)
+            .toPromise()
+            .then(response => {
+                return response
             })
     }
 }

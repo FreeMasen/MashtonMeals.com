3a8a0937a5828ca1c7b9ad784fce114479e00f0c
FreeMasen <RFM@Herman.local>
2017-01-24 21:51:49 -0600

    add queue to process multiple uploads

--- app/poster/service.ts
+++ app/poster/service.ts
@@ -9,7 +9,7 @@ import { Post } from '../models/post'
 export class Poster {
 
     private reader: FileReader = new FileReader()
-    private queue: RequestOptions[] = []
+    queue: any[] = []
     private imageHeaders: Headers = new Headers({
         'Content-Type': 'application/octet-stream'
     })
@@ -39,38 +39,70 @@ export class Poster {
     }
 
     uploadImages(images: File[]) {
-        console.log(images);
         for (var i = 0;i<images.length;i++) {
-            this.queue.push(new RequestOptions({
-                method: "POST",
-                url: `/new/image/${images[i].name}`,
-                headers: this.imageHeaders,
-                body: images[i]
-            }))
+            this.queue.push({
+                request: new RequestOptions({
+                        method: "POST",
+                        url: `/new/image/${images[i].name}`,
+                        headers: this.imageHeaders,
+                        body: images[i]}),
+                name: images[i].name,
+                size: images[i].size,
+                status: 'unsent',
+                response: ''
+            })
         }
         if (!this.clearingQueue) {
             this.clearQueue()
         }
+    }
+
+    uploadImage(image: File) {
+        return this.http.request(new Request(
+                        new RequestOptions({
+                        method: "POST",
+                        url: `/new/image/${image.name}`,
+                        headers: this.imageHeaders,
+                        body: image
+                    }
+                )
+            )
+        ).toPromise()
+        .then(response => {
+            return response
+        })
+        .catch(error => {
+            return error
+        })
     }
     
     clearQueue() {
         var self = this
         if (self.queue && self.queue.length > 0) {
             self.clearingQueue = true
-            var req = new Request(self.queue.slice(0,1)[0])
-            self.http.request(req)
+
+            var element = self.queue.filter(element => {
+                return element.status == 'unsent'
+            })[0]
+            element.status = 'sending'
+            console.log(`sending request for ${element.name}`)
+            self.http.request(new Request(element.request))
                 .toPromise()
                 .then(response => {
+                    element.status = 'completed'
+                    element.response = response.toString()
                     if (self.queue.length > 0) {
                         setTimeout(self.clearQueue, 1)
                     } else {
                         self.clearingQueue = false
                     }
+                    return element
                 })
                 .catch(error => {
                     self.clearingQueue = false
                     return error
                 })
+            return element
         }
     }
 
@@ -81,5 +113,20 @@ export class Poster {
             .then(response => {
                 return response
             })
+    }
+
+    get status(): number {
+        return this.queue.filter(element => {
+            return element.status == 'completed'
+        }).map(element => {
+            return element.size
+        }).reduce((a, b) => {
+            return a + b
+        },0) /
+        this.queue.map(element => {
+            return element.size
+        }).reduce((a, b) => {
+            return a + b
+        },0)
     }
 }

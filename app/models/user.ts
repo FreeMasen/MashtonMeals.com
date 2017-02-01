7bdd6ba824d2c7389920ca299a816fcfd7eefdec
FreeMasen <RFM@Herman.local>
2017-02-01 22:57:14 +0100

    new model

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,20 +1,24 @@
+import { AuthorizationStatus } from '../models/authorizationStatus'
 export class User {
-    firstName: string;
-    lastname: string;
-    username: string;
-    password: string;
-    emailAddress: string;
+    firstName: string
+    lastname: string
+    username: string
+    password: string
+    emailAddress: string
+    authStatus: AuthorizationStatus
     constructor(
         firstName: string = '',
         lastName: string = '',
         username: string = '',
         password: string = '',
-        emailAddress: string = ''
+        emailAddress: string = '',
+        authStatus: AuthorizationStatus = AuthorizationStatus.Unknown
     ) {
-        this.firstName = firstName;
-        this.lastname = lastName;
-        this.username = username;
-        this.password = password;
+        this.firstName = firstName
+        this.lastname = lastName
+        this.username = username
+        this.password = password
         this.emailAddress = emailAddress
+        this.authStatus = authStatus
     }
 }

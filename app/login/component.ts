07d712786a8e4f6fdf39bc50651e14706a28b00a
FreeMasen <RFM@Herman.local>
2017-02-01 22:29:06 +0100

    created login component

---
+++ app/login/component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core'
+import { OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+import { Form } from '@angular/forms'
+
+@Component({
+    selector: '<login>',
+    templateUrl: './template.html',
+    styleUrls: ['./style.css']
+})
+export class Login {
+
+    constructor() {}
+
+    ngOnInit() {
+
+    }
+}

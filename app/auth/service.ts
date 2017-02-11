a51f92398ea8b88db386186198ca0424375007c4
FreeMasen <RFM@Herman.local>
2017-02-11 12:33:52 -0500

    use cached user

--- app/auth/service.ts
+++ app/auth/service.ts
@@ -7,24 +7,48 @@ import { User } from '../models/user'
 
 @Injectable()
 export class Auth {
-
+    private user: User
     constructor(private http: Http) {}
 
     login(username: string, password: string): Promise<User> {
+        //if user has already logged in, return previously logged in
+        if (this.user) {
+            return new Promise((res, rej) => {
+                res(this.user)
+            })
+        }
         return this.http
-            .post('/auth', {username: username, password: password})
+            .post('/auth',
+                {username: username, 
+                password: password})
             .toPromise()
             .then(response => {
-                return response.json()
+                this.user = response.json()
+                return this.user
             })
     }
 
-    checkUser(): Promise<User> {
+    logout(): Promise<string> {
+        this.user = null
+        return this.http
+            .post('/logout',{})
+            .toPromise()
+            .then(response => {
+                return response.json().message
+            })
+    }
+
+    checkUser(): Promise<boolean> {
+        if (this.user) {
+            return new Promise((res, rej) => {
+                res(true)
+            })
+        }
         return this.http
             .get('/auth/user')
             .toPromise()
             .then(response => {
-                return response.json()
+                return response.json().loggedIn
             })
     }
 }

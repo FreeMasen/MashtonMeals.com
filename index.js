53997352c6ad9c4170173525995ab12a6eefa4f5
FreeMasen <RFM@Herman.local>
2017-02-11 12:30:34 -0500

    add session to express

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 const express = require('express')
 const multer = require('multer')
 const bodyParser = require('body-parser')
-const fs = require('fs')
+const morgan = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
+
 const poster = require('./src/poster.js')
+const auth = require('./src/auth.js')
+
 
 const app = express()
+var store = new MongoStore({
+  uri: 'mongodb://localhost:27017/meals',
+  collection: 'sessions'
+})
+
+store.on('error', err => {
+  console.log(err.message)
+})
 
 app.use(bodyParser.raw({limit: '5mb'}))
+app.use(bodyParser.json())
 app.use(express.static(__dirname))
+app.use(morgan('dev'))
+app.use(session({
+  secret: 'sooooo secret',
+  cookie: {
+    maxAge: 1000 * 60 * 10
+  },
+  store: store,
+  saveUninitialized: true,
+  resave: false
+}))
 
 app.get('/posts/:type/:page', (req, res) => {
   poster.getPosts(req.params.type, req.params.page ,(err, posts) => {
     if (err) res.status(500).send(err.message)
     res.send(JSON.stringify(posts))
   })
+})
+
+app.get('/auth/user', (req, res) => {
+  var status = {}
+  status.loggedIn = req.session.loggedIn 
+  res.send(JSON.stringify(status))
 })
 
 app.post('/image/', (req, res) => {
@@ -27,6 +57,15 @@ app.post('/post', (req, res) => {
   poster.newPost(req.body, (err, post) => {
     if (err) return res.status(500).send(err.message)
     res.send(JSON.stringify(post))
+  })
+})
+
+app.post('/auth', (req, res) => {
+  auth.login(req.body.username, req.body.password, (err, user) => {
+    if (err) return res.status(404).send(err.message)
+    req.session.loggedIn = true
+    req.session.username = req.body.username
+    res.send(JSON.stringify(user))
   })
 })
 

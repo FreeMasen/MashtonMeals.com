b4fbccaa9a08626e4d64826106990a21d5e20a5a
FreeMasen <RFM@Herman.local>
2017-02-11 12:36:49 -0500

    flesh out actions

--- app/login/component.ts
+++ app/login/component.ts
@@ -1,18 +1,36 @@
 import { Component } from '@angular/core'
 import { OnInit } from '@angular/core'
 import { Router } from '@angular/router'
+import { Location } from '@angular/common'
 import { Form } from '@angular/forms'
+
+import { User } from '../models/user'
+import { Auth } from '../auth/service'
+import { Messenger } from '../messenger/service'
 
 @Component({
     selector: '<login>',
-    templateUrl: './template.html',
-    styleUrls: ['./style.css']
+    templateUrl: 'app/login/template.html',
+    styleUrls: ['app/login/style.css']
 })
 export class Login {
+    username:string = ''
+    password: string = ''
+    constructor(private auth: Auth,
+                private location: Location,
+                private router: Router,
+                private messenger: Messenger) {}
 
-    constructor() {}
+    login() {
+        this.auth.login(this.username, this.password)
+            .then(user => {
+                this.router.navigate(['dashboard'])
+            }).catch(error => {
+                this.messenger.display(error.message || 'Unknown error occured')
+            })
+    }
 
-    ngOnInit() {
-
+    goBack() {
+        this.location.back()
     }
 }

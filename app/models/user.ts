c80c8c03d2e6969e9cce3f01488b964a9fbdcf2e
FreeMasen <RFM@Herman.local>
2017-02-11 12:37:37 -0500

    remove password from model

--- app/models/user.ts
+++ app/models/user.ts
@@ -3,21 +3,18 @@ export class User {
     firstName: string
     lastname: string
     username: string
-    password: string
     emailAddress: string
     authStatus: AuthorizationStatus
     constructor(
         firstName: string = '',
         lastName: string = '',
         username: string = '',
-        password: string = '',
         emailAddress: string = '',
         authStatus: AuthorizationStatus = AuthorizationStatus.Unknown
     ) {
         this.firstName = firstName
         this.lastname = lastName
         this.username = username
-        this.password = password
         this.emailAddress = emailAddress
         this.authStatus = authStatus
     }

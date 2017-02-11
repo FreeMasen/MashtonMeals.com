cacfc29d950808949f89986661215fef7b77c132
FreeMasen <RFM@Herman.local>
2017-02-11 12:38:11 -0500

    Addd list route

--- app/router/module.ts
+++ app/router/module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router'
 import { Dashboard } from '../dashboard/component'
 import { Entry } from '../entry/component'
 import { Login } from '../login/component'
+import { List } from '../list/component'
 
 const routes: Routes = [
     {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     {path: 'dashboard', component: Dashboard},
     {path: 'entry', component: Entry},
-    {path: 'login', component: Login}
+    {path: 'login', component: Login},
+    {path: 'list/:type/:page', component: List}
 ]
 
 @NgModule({

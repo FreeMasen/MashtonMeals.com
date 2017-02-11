96aab3b586bd9f619f725bc9dd2fd66a4032345c
FreeMasen <RFM@Herman.local>
2017-02-11 12:38:30 -0500

    add server side authentication

---
+++ src/auth.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcrypt')
+const mongo = require('mongojs')
+const db = mongo('meals', ['auth', 'sessions', 'users'])
+const User = require('../app/models/user.js')
+
+function Auth() {}
+
+Auth.prototype.login = function(username, password, cb) {
+    db.auth.findOne({username: username}, (err, doc) => {
+        if (err) return cb(err)
+        if (!doc) return cb(new Error('Unknown User'))
+        bcrypt.compare(password, doc.password, (err, match) => {
+            if (err) return cb(err)
+            if (!match) return cb(new Error('Incorrect password'))
+            db.users.find({username: username}, (err, doc) => {
+                if (err) return cb(err)
+                if (!doc) return cb(new Error('Critical DB error, users and auth are inconsistant'))
+                cb(null, doc)
+            })
+        })
+    })
+}
+
+Auth.prototype.logout = function(username, sessionId) {
+    db.sessions.findAndModify({sessionId: sessionId, username: username})
+}
+
+module.exports = new Auth()

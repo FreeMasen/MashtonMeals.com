b72fc80500d0357e72da2821baabe7419e9d59bf
FreeMasen <RFM@Herman.local>
2017-02-12 21:06:03 -0600

    move contents partitioning to client side

--- src/poster.js
+++ src/poster.js
@@ -7,10 +7,9 @@ function Poster() {
 
 }
 Poster.prototype.newPost = function(post, cb) {
-    post.contents = post.contents[0].split('##')
     db.posts.save(post, (err, doc) => {
         if (err) return cb(err)
-        cb(null, doc)
+        cb(null)
     })
 }
 
@@ -27,9 +26,20 @@ Poster.prototype.getPosts = function(type, page, cb) {
         q.type = type
     }
     if (typeof page == 'function') page = 0
-    db.posts.find(q).limit(10).skip(page * 10, (err, docs) => {
+    db.posts.find(q).limit(9).skip(page * 9, (err, docs) => {
         if (err) return cb(err)
         cb(null, docs)
+    })
+}
+
+Poster.prototype.getPost = function(id, cb) {
+    var q = {
+        _id: id
+    }
+    db.posts.findOne(q, (err, doc) => {
+        if (err) return cb(err)
+        if (!doc) return cb(new Error('Unable to find post with that id'))
+        cb(null, doc)
     })
 }
 
@@ -56,43 +66,3 @@ Poster.prototype.removePost = function(id, db) {
 }
 
 module.exports = new Poster()
-
-function seed() {
-    var bulk = db.posts.initializeOrderedBulkOp()
-    var posts = generatePosts()
-    posts.forEach(post => {
-        bulk.insert(post)
-    })
-    bulk.execute((err, res) => {
-        if (err) return console.log(err.message)
-        console.log(res)
-    })
-}
-
-function generatePosts() {
-    var ret = []
-    var lorem = [`On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.`]
-    
-    var dateCounter = 0
-    for (i=0;i<10;i++) {
-      dateCounter += 1
-      ret.push({
-        _id: i,
-        title: `${i}: recipe`,
-        type: 'recipe',
-        postDate: new Date(2017,01,dateCounter),
-        contents: lorem,
-        images: ['assets/images/test.jpg']
-      })
-      dateCounter += 1
-      ret.push({
-        _id: i+10,
-        title: `${i} review`,
-        type: 'review',
-        postDate: new Date(2017, 01, dateCounter),
-        contents: lorem,
-        images: ['assets/images/test.jpg']
-      })
-    }
-    return ret
-}

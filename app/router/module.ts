a6b9b009056c93332ee73460325d6e00621ec755
FreeMasen <RFM@Herman.local>
2017-02-15 12:56:13 -0600

    add single component to router

--- app/router/module.ts
+++ app/router/module.ts
@@ -5,13 +5,15 @@ import { Dashboard } from '../dashboard/component'
 import { Entry } from '../entry/component'
 import { Login } from '../login/component'
 import { List } from '../list/component'
+import { Single } from '../single/component'
 
 const routes: Routes = [
     {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     {path: 'dashboard', component: Dashboard},
     {path: 'entry', component: Entry},
     {path: 'login', component: Login},
-    {path: 'list/:type/:page', component: List}
+    {path: 'list/:type/:page', component: List},
+    {path: 'single/:id', component: Single}
 ]
 
 @NgModule({

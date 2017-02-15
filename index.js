32572ad28b66104b821326ad5ae9fb482a597337
FreeMasen <RFM@Herman.local>
2017-02-15 13:27:56 -0600

    remove unneeded dep

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const multer = require('multer')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const session = require('express-session')
